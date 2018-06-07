846344db788d668c5d573a5cd3fea751c21e228b
Aarthi Ram <aaramamu@cisco.com>
2018-06-07 13:03:05 -0500

    Python SDK for DNAC

---
+++ python_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from python_client.api.discovery_api import DiscoveryApi
+from python_client.api.dna_intent_api import DnaIntentApi
+from python_client.api.file_api import FileApi
+from python_client.api.flow_analysis_api import FlowAnalysisApi
+from python_client.api.global_credential_api import GlobalCredentialApi
+from python_client.api.image_api import ImageApi
+from python_client.api.interface_api import InterfaceApi
+from python_client.api.misc_api import MiscApi
+from python_client.api.network_device_api import NetworkDeviceApi
+from python_client.api.onboarding_api import OnboardingApi
+from python_client.api.task_api import TaskApi
+from python_client.api.template_programmer_api import TemplateProgrammerApi
+from python_client.api.topology_api import TopologyApi
